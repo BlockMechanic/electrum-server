9ec54ff695da31d36c76953e26caa67796ff7abb
thelonecrouton <thelonecrouton@gmail.com>
2015-07-19 18:31:55 +0100

    chow mein

--- src/deserialize.py
+++ src/deserialize.py
@@ -250,7 +250,7 @@ def parse_Transaction(vds, is_coinbase):
 
 
 opcodes = Enumeration("Opcodes", [
-    ("OP_0", 0x00), ("OP_PUSHDATA1", 0x4c), ("OP_PUSHDATA2" 0x4d), ("OP_PUSHDATA4" 0x4e), ("OP_1NEGATE" 0x4f), ("OP_RESERVED" 0x50),
+    ("OP_0", 0x00), ("OP_PUSHDATA1", 0x4c), ("OP_PUSHDATA2", 0x4d), ("OP_PUSHDATA4", 0x4e), ("OP_1NEGATE", 0x4f), ("OP_RESERVED", 0x50),
     "OP_1", "OP_2", "OP_3", "OP_4", "OP_5", "OP_6", "OP_7",
     "OP_8", "OP_9", "OP_10", "OP_11", "OP_12", "OP_13", "OP_14", "OP_15", "OP_16",
     "OP_NOP", "OP_VER", "OP_IF", "OP_NOTIF", "OP_VERIF", "OP_VERNOTIF", "OP_ELSE", "OP_ENDIF", "OP_VERIFY",
